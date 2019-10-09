48f3a51aab4c4101094df64979fdcf2e9549b672
Espoir Murhabazi <espoir.mur@gmail.com>
2019-10-09 02:00:07 +0200

    add update url endpoint

--- GithubAPI/urls.py
+++ GithubAPI/urls.py
@@ -4,12 +4,13 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.views.generic.base import RedirectView
 from RestAPI.views import (delete_all_events,
                            GetPostEventView,
-                           get_actors)
+                           ActorView, get_actors_by_longest_streak)
 
 urlpatterns = [
     # Dummy route. Can be removed.
     url(r'^admin/', admin.site.urls),
     url(r'^erase/', delete_all_events, name='delete-all'),
     url(r'^events/', include('RestAPI.urls')),
-    url(r'^actors/', get_actors)
+    url(r'^actors/', ActorView.as_view()),
+    #url(r'^actors/streak', get_actors_by_longest_streak)
 ]
--- RestAPI/views.py
+++ RestAPI/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Event, Actor
 from .serializers import EventSerializer, ActorSerializer
 from rest_framework.response import Response
@@ -45,23 +46,70 @@ def get_events_by_actor_id(request, actor_id):
         return Response(serializer.data)
 
 
+class ActorView(APIView):
+    serializer = ActorSerializer
+
+    def get(self, request):
+        raw_query = '''
+        select RestAPI_actor.id,
+            RestAPI_actor.login,
+            RestAPI_actor.avatar_url,
+            count(RestAPI_event.id) as events_organized
+            from RestAPI_actor
+            inner join RestAPI_event
+            on RestAPI_event.actor_id = RestAPI_actor.id
+            group by RestAPI_event.actor_id
+            order by events_organized desc,
+                        RestAPI_event.created_at desc,
+                        RestAPI_actor.login desc
+                    '''
+        actors = Actor.objects.raw(raw_query)
+        serializer = ActorSerializer(actors, many=True)
+        return Response(serializer.data)
+
+    def put(self, request):
+        actor_data = request.data
+        serializer = ActorSerializer(data=actor_data)
+        serializer.is_valid(raise_exception=True)
+        actor_data = serializer.data
+        actor = get_object_or_404(Actor, pk=actor_data.get('id'))
+        if actor.login != actor_data.get('login'):
+            return Response(status=status.HTTP_400_BAD_REQUEST,
+                            data={'message': "should not update the login"})
+        actor.avatar_url = actor_data.get("avatar_url")
+        actor.save()
+        return Response(serializer.data)
+
+
+
+def dictfetchall(query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        desc = cursor.description
+        return [
+            dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()
+        ]
 
 
 @api_view(['GET'])
-def get_actors(request):
+def get_actors_by_longest_streak(request):
     raw_query = '''
     select RestAPI_actor.id,
            RestAPI_actor.login,
            RestAPI_actor.avatar_url,
-           count(RestAPI_event.id) as events_organized
-           from RestAPI_actor
-           inner join RestAPI_event
-           on RestAPI_event.actor_id = RestAPI_actor.id
-           group by RestAPI_event.actor_id
-           order by events_organized desc,
-                    RestAPI_event.created_at desc,
-                    RestAPI_actor.login desc
+           count(*) as streak,
+           min RestAPI_event.created_at as from_date,
+           max RestAPI_event.created_at  as to_date
+           from (select RestAPI_event.created_at,
+                 RestAPI_event.actor_id,
+                 row_number() over 
+                 (partition by RestAPI_event.actor_id order by RestAPI_event.created_at)
+                  as seqnum
+                  from RestAPI_actor
+                  inner join RestAPI_event
+                  on RestAPI_event.actor_id = RestAPI_actor.id
+           ) RestAPI_event.actor
+           group by RestAPI_event.actor_id, ( RestAPI_event.created_at - seqnum)
                 '''
-    actors = Actor.objects.raw(raw_query)
-    serializer = ActorSerializer(actors, many=True)
-    return Response(serializer.data)
+    actors = dictfetchall(raw_query)
+    return Response(actors)
